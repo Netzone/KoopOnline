a8fa2dc1749142c1854dd65ff27b6f9d442f68bc
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 12:40:20 +0100

    commented tags in routes because it's not needed anymore

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #Set suppliers#index as root route
   root 'suppliers#index'
-  resources :tags
+  #resources :tags
   resources :suppliers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
