d1f55dd9c7fab7e2cfc7cffcf722f9973a4f2f1b
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 12:50:54 +0100

    Added devise views and changed the registration view to add address and city upon registering, also added those values to devise sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  #Address and city on registering account
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:address, :city, :email, :password, :password_confirmation) }
+  end
+
+  protect_from_forgery with: :exception
 end
