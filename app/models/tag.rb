b6dd8c965ef736d8301a659f4e39748c53eca261
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 14:50:15 +0100

    Tested create tag without tag name, and added validation in tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :tags_suppliers
   has_many :suppliers, :through => :tags_suppliers
+  validates :tag, presence: true
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should not create tag without tag name" do
+     tag = Tag.new({tag:''})
+     assert_not tag.save
+   end
 end
