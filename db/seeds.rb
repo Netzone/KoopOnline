769b9f371a1dbaa795416b08f003cfff14cfde99
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 15:33:15 +0100

    Finishing up seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,8 @@ user = User.new(
     city: 'Jönköping',
     password_confirmation: 'admin123'
 )
-user.skip_confirmation!
 user.save!
 
-Supplier.new({name:'Orkla Foods', email:'info@orklafoods.se', address:'Isbergs gata 9B', city:'Malmö', website:'orklafoods.se', phone:'010-142 42 42', user_id: 1, all_tags:'meat,ketchup, microwave'})
-Supplier.new({name:'Åre Bageri', email:'info@arebageri.se', address:'Årevägen 55', city:'Åre', website:'arebageri.se', phone:'0647-52320', user_id: users(:tom).id, all_tags:'bread,deli'})
-Supplier.new({name:'Elgiganten', email:'info@elgiganten.se', address:'Möbelvägen 52', city:'Jönköping', website:'elgiganten.se', phone:'1234568', user_id: users(:tom).id, all_tags:'technology'})
+Supplier.create!({name:'Orkla Foods', email:'info@orklafoods.se', address:'Isbergs gata 9B', city:'Malmö', website:'orklafoods.se', phone:'010-142 42 42', user_id: 1, all_tags:'meat,ketchup, microwave'})
+Supplier.create!({name:'Åre Bageri', email:'info@arebageri.se', address:'Årevägen 55', city:'Åre', website:'arebageri.se', phone:'0647-52320', user_id: 1, all_tags:'bread,deli'})
+Supplier.create!({name:'Elgiganten', email:'info@elgiganten.se', address:'Möbelvägen 52', city:'Jönköping', website:'elgiganten.se', phone:'1234568', user_id: 1, all_tags:'technology'})
