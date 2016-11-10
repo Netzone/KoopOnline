868293fb772dbfc1247b8245cc28c4ee0d5c17c1
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 15:43:59 +0100

    Added test for valid user testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "valid user" do
+    hash = {email: 'koop@koop.se',
+            password: 'admin123',
+            address:'Vintergatan 7',
+            city: 'Jönköping',
+            password_confirmation: 'admin123'}
+    user = User.new hash
+    assert user.valid?
+  end
 end
