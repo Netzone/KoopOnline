612757204911515af07bf8dab5398ddd55a251aa
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 14:25:41 +0100

    first supplier test ran

--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class SupplierTest < ActiveSupport::TestCase
-   test "should not create new supplier without address" do
-     supplier = Supplier.new
+   test "should not create new supplier without user_id" do
+     supplier = Supplier.new({name:'test', email:'d', address:'d', city:'d', website:'d', phone:'d'})
      assert_not supplier.save
    end
 end
