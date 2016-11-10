67aff4580f9c6d3b34186b5ebf79406c9fae3399
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 13:41:36 +0100

    cleanup a bit in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
   #Address and city on registering account
+  protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:address, :city, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :city, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :city, :email, :password, :password_confirmation, :current_password])
   end
 
   protect_from_forgery with: :exception
