0fe170b04bf3861548927a2006a17581bdee5a1a
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 12:35:15 +0100

    Added comments for self clarification

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,6 +3,8 @@ class Supplier < ApplicationRecord
   has_many :tags, :through => :tags_suppliers
 
   #Virtual attributes -- First time using
+  #When attribute :all_tags is called, call these functions on the values before adding it to the database?
+  #Must be like that...
   def all_tags=(tags)
     self.tags = tags.split(",").map do |tag|
       Tag.where(tag: tag.strip).first_or_create!
