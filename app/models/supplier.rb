a9a0c1f94747095a282bac7bb068f3e70632edfe
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 14:46:58 +0100

    Checking validations for supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,6 +2,12 @@ class Supplier < ApplicationRecord
   belongs_to :user
   has_many :tags_suppliers
   has_many :tags, :through => :tags_suppliers
+  validates :website, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 
   #Virtual attributes -- First time using
   #When attribute :all_tags is called, call these functions on the values before adding it to the database?
--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -10,4 +10,28 @@ class SupplierTest < ActiveSupport::TestCase
     supplier = Supplier.new({name:'test', email:'d', address:'d', city:'d', website:'d', phone:'d', user_id: users(:tom).id})
     assert supplier.save
   end
+  test "should not create new supplier without address" do
+    supplier = Supplier.new({name:'test', email:'d', address:'', city:'d', website:'d', phone:'d', user_id: users(:tom).id})
+    assert_not supplier.save
+  end
+   test "should not create new supplier without name" do
+     supplier = Supplier.new({name:'', email:'d', address:'d', city:'d', website:'d', phone:'d', user_id: users(:tom).id})
+     assert_not supplier.save
+   end
+   test "should not create new supplier without email" do
+     supplier = Supplier.new({name:'test', email:'', address:'d', city:'d', website:'d', phone:'d', user_id: users(:tom).id})
+     assert_not supplier.save
+   end
+   test "should not create new supplier without city" do
+     supplier = Supplier.new({name:'test', email:'d', address:'d', city:'', website:'d', phone:'d', user_id: users(:tom).id})
+     assert_not supplier.save
+   end
+   test "should not create new supplier without website" do
+     supplier = Supplier.new({name:'test', email:'d', address:'d', city:'d', website:'', phone:'d', user_id: users(:tom).id})
+     assert_not supplier.save
+   end
+   test "should not create new supplier without phone" do
+     supplier = Supplier.new({name:'test', email:'d', address:'d', city:'d', website:'d', phone:'', user_id: users(:tom).id})
+     assert_not supplier.save
+   end
 end
