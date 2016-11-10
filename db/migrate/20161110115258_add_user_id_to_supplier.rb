0c52c1413abd5fac9e797ee5ee6b624ddde09d53
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 13:02:24 +0100

    fixed migration again

--- db/migrate/20161110115258_add_user_id_to_supplier.rb
+++ db/migrate/20161110115258_add_user_id_to_supplier.rb
@@ -1,5 +1,5 @@
 class AddUserIdToSupplier < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :user_id, :integer, :null => false
+    add_column :users, :user_id, :integer, :null => false, :default => 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110111110) do
+ActiveRecord::Schema.define(version: 20161110115258) do
 
   create_table "suppliers", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161110111110) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "city"
+    t.integer  "user_id",                default: 0,  null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
