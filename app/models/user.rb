16c5759cc048ebe866ed4dcd5aa1f0103bc3db56
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 15:46:55 +0100

    Added user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :suppliers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :address, presence: true
+  validates :city, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,24 @@ class UserTest < ActiveSupport::TestCase
     user = User.new hash
     assert user.valid?
   end
+
+  test "should not save user without address" do
+    hash = {email: 'koop@koop.se',
+            password: 'admin123',
+            address:'',
+            city: 'Jönköping',
+            password_confirmation: 'admin123'}
+    user = User.new hash
+    assert_not user.valid?
+  end
+
+  test "should not save user without city" do
+    hash = {email: 'koop@koop.se',
+            password: 'admin123',
+            address:'Vintergatan 7',
+            city: '',
+            password_confirmation: 'admin123'}
+    user = User.new hash
+    assert_not user.valid?
+  end
 end
