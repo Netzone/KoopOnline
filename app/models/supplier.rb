feb998fd78d9385725f987a710ba0622fda21505
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-11-10 12:25:04 +0100

    Added virtual attributes for creating and getting tags from supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,16 @@
 class Supplier < ApplicationRecord
   has_many :tags_suppliers
   has_many :tags, :through => :tags_suppliers
+
+  #Virtual attributes -- First time using
+  def all_tags=(tags)
+    self.tags = tags.split(",").map do |name|
+      Tag.where(tag: tag.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&tag).join(", ")
+  end
+
 end
